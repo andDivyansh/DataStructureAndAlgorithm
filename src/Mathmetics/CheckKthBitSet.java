41d9ff1786344de353b1166de21d797eb6e992d1
Divyansh <54066102+andDivyansh@users.noreply.github.com>
2021-07-10 00:22:24 +0530

    Check kth Bit is set

---
+++ src/Mathmetics/CheckKthBitSet.java
@@ -0,0 +1,18 @@
+package Mathmetics;
+
+public class CheckKthBitSet {
+
+    public static void isKthBitSet(int number, int bitNumber) {
+        // Move the bit  to the last and check with '&' operator
+        // if the last bit is 1 then it returns 1 else returns 0
+        if (((number >> (bitNumber - 1)) & 1) == 1) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+
+    public static void main(String[] args) {
+        isKthBitSet(10 , 3);
+    }
+}
