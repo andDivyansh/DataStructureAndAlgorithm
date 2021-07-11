b9d895a4a69f864bc9cf961f57a49d9a2d1e080d
Divyansh <54066102+andDivyansh@users.noreply.github.com>
2021-07-11 14:29:53 +0530

    Missing Number in Arrays using XOR operator

---
+++ src/Arrays/MissingNumberInArray.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+public class MissingNumberInArray {
+
+    public static int findMissingNumber(int arr[]) {
+        int xorResult =  0;
+        // First take the xor of all numbers equals to size of the array.
+        for (int index = 1; index <= (arr.length +1); index++) {
+            xorResult = xorResult ^ index;
+        }
+
+        // xor all the elements off the array
+        for (int secondIndex = 0; secondIndex < arr.length; secondIndex++){
+            xorResult =  xorResult ^ arr[secondIndex];
+        }
+        return xorResult;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 4, 1, 5};
+        System.out.println("Missing number in array is " + findMissingNumber(arr));
+    }
+}
