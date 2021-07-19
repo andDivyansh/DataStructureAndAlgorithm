9d2dd5c7789d1e7292bcdef9751558177c0e9e63
Divyansh <54066102+andDivyansh@users.noreply.github.com>
2021-07-19 18:44:22 +0530

    Sum Of numbers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"+ Math.pow(2, 3));
     }
 }
